8fa09d4af6f03483bcb7c0ada31e6942a099b1ab
Ignacio Guillen <ignacio.guillen00@gmail.com>
2023-05-06 03:42:07 -0300

    form compra con func de compra

--- Carniceria/Vista/Frm_Compra.Designer.cs
+++ Carniceria/Vista/Frm_Compra.Designer.cs
@@ -34,11 +34,13 @@
             this.corteCarne = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btn_costillar = new System.Windows.Forms.Button();
-            this.btn_chorizo = new System.Windows.Forms.Button();
-            this.btn_tiraAsado = new System.Windows.Forms.Button();
-            this.btn_vacio = new System.Windows.Forms.Button();
-            this.btn_bondiola = new System.Windows.Forms.Button();
+            this.tb_kg = new System.Windows.Forms.TextBox();
+            this.rtb_cuenta = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clb_carnes = new System.Windows.Forms.CheckedListBox();
+            this.lb_total = new System.Windows.Forms.Label();
+            this.btn_anotarPedido = new System.Windows.Forms.Button();
+            this.btn_anularCompra = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_listaCarnes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +48,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(300, 9);
+            this.label1.Location = new System.Drawing.Point(222, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(190, 37);
             this.label1.TabIndex = 1;
@@ -57,13 +59,13 @@
             // 
             this.lb_agregarCarnes.AutoSize = true;
             this.lb_agregarCarnes.BackColor = System.Drawing.Color.Transparent;
-            this.lb_agregarCarnes.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.lb_agregarCarnes.ForeColor = System.Drawing.Color.Blue;
-            this.lb_agregarCarnes.Location = new System.Drawing.Point(45, 98);
+            this.lb_agregarCarnes.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.lb_agregarCarnes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lb_agregarCarnes.Location = new System.Drawing.Point(87, 57);
             this.lb_agregarCarnes.Name = "lb_agregarCarnes";
-            this.lb_agregarCarnes.Size = new System.Drawing.Size(134, 20);
+            this.lb_agregarCarnes.Size = new System.Drawing.Size(84, 15);
             this.lb_agregarCarnes.TabIndex = 14;
-            this.lb_agregarCarnes.Text = "ESCOGER CARNE";
+            this.lb_agregarCarnes.Text = "ELEGIR CARNE";
             // 
             // dgv_listaCarnes
             // 
@@ -72,7 +74,7 @@
             this.corteCarne,
             this.cantidad,
             this.precio});
-            this.dgv_listaCarnes.Location = new System.Drawing.Point(47, 195);
+            this.dgv_listaCarnes.Location = new System.Drawing.Point(12, 288);
             this.dgv_listaCarnes.Name = "dgv_listaCarnes";
             this.dgv_listaCarnes.RowTemplate.Height = 25;
             this.dgv_listaCarnes.Size = new System.Drawing.Size(369, 150);
@@ -99,68 +101,87 @@
             this.precio.Name = "precio";
             this.precio.ReadOnly = true;
             // 
-            // btn_costillar
+            // tb_kg
             // 
-            this.btn_costillar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.btn_costillar.Location = new System.Drawing.Point(347, 130);
-            this.btn_costillar.Name = "btn_costillar";
-            this.btn_costillar.Size = new System.Drawing.Size(69, 35);
-            this.btn_costillar.TabIndex = 12;
-            this.btn_costillar.Text = "Costillar";
-            this.btn_costillar.UseVisualStyleBackColor = false;
+            this.tb_kg.Location = new System.Drawing.Point(87, 202);
+            this.tb_kg.Name = "tb_kg";
+            this.tb_kg.Size = new System.Drawing.Size(131, 23);
+            this.tb_kg.TabIndex = 15;
             // 
-            // btn_chorizo
+            // rtb_cuenta
             // 
-            this.btn_chorizo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.btn_chorizo.Location = new System.Drawing.Point(272, 130);
-            this.btn_chorizo.Name = "btn_chorizo";
-            this.btn_chorizo.Size = new System.Drawing.Size(69, 35);
-            this.btn_chorizo.TabIndex = 11;
-            this.btn_chorizo.Text = "Chorizo";
-            this.btn_chorizo.UseVisualStyleBackColor = false;
+            this.rtb_cuenta.Location = new System.Drawing.Point(467, 64);
+            this.rtb_cuenta.Name = "rtb_cuenta";
+            this.rtb_cuenta.Size = new System.Drawing.Size(202, 300);
+            this.rtb_cuenta.TabIndex = 16;
+            this.rtb_cuenta.Text = "";
             // 
-            // btn_tiraAsado
+            // label2
             // 
-            this.btn_tiraAsado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.btn_tiraAsado.Location = new System.Drawing.Point(197, 130);
-            this.btn_tiraAsado.Name = "btn_tiraAsado";
-            this.btn_tiraAsado.Size = new System.Drawing.Size(69, 35);
-            this.btn_tiraAsado.TabIndex = 10;
-            this.btn_tiraAsado.Text = "Asado";
-            this.btn_tiraAsado.UseVisualStyleBackColor = false;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Cantidad (kg) gramos en \",\": ";
             // 
-            // btn_vacio
+            // clb_carnes
             // 
-            this.btn_vacio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.btn_vacio.Location = new System.Drawing.Point(122, 130);
-            this.btn_vacio.Name = "btn_vacio";
-            this.btn_vacio.Size = new System.Drawing.Size(69, 35);
-            this.btn_vacio.TabIndex = 9;
-            this.btn_vacio.Text = "Vacio";
-            this.btn_vacio.UseVisualStyleBackColor = false;
+            this.clb_carnes.FormattingEnabled = true;
+            this.clb_carnes.Items.AddRange(new object[] {
+            "Bondiola",
+            "Vacio",
+            "Tira de Asado",
+            "Chorizo",
+            "Costillar"});
+            this.clb_carnes.Location = new System.Drawing.Point(87, 75);
+            this.clb_carnes.Name = "clb_carnes";
+            this.clb_carnes.Size = new System.Drawing.Size(120, 94);
+            this.clb_carnes.TabIndex = 18;
             // 
-            // btn_bondiola
+            // lb_total
             // 
-            this.btn_bondiola.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.btn_bondiola.Location = new System.Drawing.Point(47, 130);
-            this.btn_bondiola.Name = "btn_bondiola";
-            this.btn_bondiola.Size = new System.Drawing.Size(69, 35);
-            this.btn_bondiola.TabIndex = 8;
-            this.btn_bondiola.Text = "Bondiola";
-            this.btn_bondiola.UseVisualStyleBackColor = false;
+            this.lb_total.AutoSize = true;
+            this.lb_total.Location = new System.Drawing.Point(496, 46);
+            this.lb_total.Name = "lb_total";
+            this.lb_total.Size = new System.Drawing.Size(41, 15);
+            this.lb_total.TabIndex = 19;
+            this.lb_total.Text = "Total : ";
+            // 
+            // btn_anotarPedido
+            // 
+            this.btn_anotarPedido.Location = new System.Drawing.Point(87, 248);
+            this.btn_anotarPedido.Name = "btn_anotarPedido";
+            this.btn_anotarPedido.Size = new System.Drawing.Size(120, 23);
+            this.btn_anotarPedido.TabIndex = 20;
+            this.btn_anotarPedido.Text = "AGREGAR CARNE";
+            this.btn_anotarPedido.UseVisualStyleBackColor = true;
+            this.btn_anotarPedido.Click += new System.EventHandler(this.btn_anotarPedido_Click);
+            // 
+            // btn_anularCompra
+            // 
+            this.btn_anularCompra.Location = new System.Drawing.Point(222, 248);
+            this.btn_anularCompra.Name = "btn_anularCompra";
+            this.btn_anularCompra.Size = new System.Drawing.Size(120, 23);
+            this.btn_anularCompra.TabIndex = 21;
+            this.btn_anularCompra.Text = "ANULAR COMPRA";
+            this.btn_anularCompra.UseVisualStyleBackColor = true;
+            this.btn_anularCompra.Click += new System.EventHandler(this.button1_Click);
             // 
             // Frm_Compra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_anularCompra);
+            this.Controls.Add(this.btn_anotarPedido);
+            this.Controls.Add(this.lb_total);
+            this.Controls.Add(this.clb_carnes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtb_cuenta);
+            this.Controls.Add(this.tb_kg);
             this.Controls.Add(this.lb_agregarCarnes);
             this.Controls.Add(this.dgv_listaCarnes);
-            this.Controls.Add(this.btn_costillar);
-            this.Controls.Add(this.btn_chorizo);
-            this.Controls.Add(this.btn_tiraAsado);
-            this.Controls.Add(this.btn_vacio);
-            this.Controls.Add(this.btn_bondiola);
             this.Controls.Add(this.label1);
             this.Name = "Frm_Compra";
             this.Text = "Frm_Compra";
@@ -177,10 +198,14 @@
         private DataGridViewTextBoxColumn corteCarne;
         private DataGridViewTextBoxColumn cantidad;
         private DataGridViewTextBoxColumn precio;
-        private Button btn_costillar;
-        private Button btn_chorizo;
-        private Button btn_tiraAsado;
-        private Button btn_vacio;
-        private Button btn_bondiola;
+        private TextBox tb_kg;
+        private RichTextBox rtb_cuenta;
+        private Label label2;
+        private CheckedListBox clb_carnes;
+        private Label label3;
+        private Button btn_anotarPedido;
+        private Button button1;
+        private Button btn_anularCompra;
+        private Label lb_total;
     }
 }
--- Carniceria/Vista/Frm_Compra.cs
+++ Carniceria/Vista/Frm_Compra.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Carniceria;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,24 +8,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Vista
 {
     public partial class Frm_Compra : Form
     {
+        decimal cuenta = 0;
+        decimal total = 0;
+        decimal kilosLlevados= 0;
         public Frm_Compra()
         {
             InitializeComponent();
+            ActualizarListas();
         }
 
-        private void Frm_Compra_FormClosing(object sender, FormClosingEventArgs e)
+        public void ActualizarListas()
         {
-            this.Hide();
+            dgv_listaCarnes.Rows.Clear();
+            for (int i = 0; i < Negocio.Heladera.ListaCarnes.Count; i++)
+            {
+                int n = dgv_listaCarnes.Rows.Add();
+
+                dgv_listaCarnes.Rows[n].Cells[0].Value = Negocio.Heladera.ListaCarnes[i].CorteDeCarne;
+                dgv_listaCarnes.Rows[n].Cells[1].Value = Negocio.Heladera.ListaCarnes[i].Stock;
+                dgv_listaCarnes.Rows[n].Cells[2].Value = Negocio.Heladera.ListaCarnes[i].PrecioPorKilo;
+            }
         }
 
-        private void Frm_Compra_FormClosed(object sender, FormClosedEventArgs e)
+        private void btn_anotarPedido_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            if (!string.IsNullOrEmpty(tb_kg.Text))
+            {
+                kilosLlevados += Decimal.Parse(tb_kg.Text);
+                foreach (var item in clb_carnes.CheckedItems)
+                {
+                    foreach (var tipoCarne in Negocio.Heladera.ListaCarnes)
+                    {
+                        if (tipoCarne.CorteDeCarne == item.ToString())
+                        {
+                            
+                            cuenta = tipoCarne.PrecioPorKilo * Decimal.Parse(tb_kg.Text);
+                            total += cuenta;
+                            rtb_cuenta.AppendText(item.ToString() + kilosLlevados + " kg" + "\n");
+                        }
+                    }
+                    rtb_cuenta.AppendText(cuenta + "\n");
+                }
+                rtb_cuenta.AppendText("----------------------------\n");
+                rtb_cuenta.AppendText("Esta gastando: " + total + "$" + "\n");
+                rtb_cuenta.AppendText("----------------------------\n");
+                lb_total.Text = "total: " + total + " PESOS";
+            }
+            else
+            {
+                MessageBox.Show("ERROR NO COLOCO NINGUNA CANTIDAD EN KILOS!", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("DESEA ANULAR LA COMPRA? SE BORRARA TODO EL PEDIDO MARCADO", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (resultado == DialogResult.Yes)
+            {
+                rtb_cuenta.Clear();
+                lb_total.Text = "total: ";
+                cuenta = 0;
+                total = 0;
+                kilosLlevados = 0;
+            }
         }
     }
 }
